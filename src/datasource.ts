9d5b3e7910257f2da4e0d34f0dc3a8f117ce82c3
Sean Dague <sean.dague@ibm.com>
2023-05-15 13:59:18 -0400

    use stored endpoint
    sort of working
    
    Add .whitesource configuration file
    
    Fix promises logic - thanks to Ashley!
    
    add variable support
    
    make this signable
    
    update logdna datasource with app tags
    
    Update docker version
    
    upgrade to 9.5.2 for dev
    
    fix the easy fixes
    
    upgrade to grafana 10 as dependency
    
    remove e2e test package which drags in sec vulns
    
    update license stanzas

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,3 +1,15 @@
+/**
+ * (C) Copyright IBM 2023.
+ *
+ * This code is licensed under the Apache License, Version 2.0. You may
+ * obtain a copy of this license in the LICENSE file in the root directory
+ * of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Any modifications or derivative works of this code must retain this
+ * copyright notice, and modified files need to carry a notice indicating
+ * that they have been altered from the originals.
+ */
+
 import {
   DataQueryRequest,
   DataQueryResponse,
@@ -7,7 +19,7 @@ import {
   FieldType,
 } from '@grafana/data';
 
-import { getBackendSrv } from '@grafana/runtime';
+import { getBackendSrv, getTemplateSrv } from '@grafana/runtime';
 
 import { MyQuery, MyDataSourceOptions } from './types';
 
@@ -21,15 +33,13 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     this.url = instanceSettings.url;
   }
 
-  async doRequest(path: any, query: any) {
-
+  async doRequest(path: any, params?: any) {
     const req = {
       method: "GET",
-      url: this.url + "/example" + path
+      url: this.url + "/example" + path,
+      params
     }
-    console.log(req)
-    const result = await getBackendSrv().datasourceRequest(req)
-    console.log(result)
+    const result = await getBackendSrv().datasourceRequest(req);
     return result;
   }
 
@@ -37,25 +47,36 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     const { range } = options;
     const from = range!.from.valueOf();
     const to = range!.to.valueOf();
+    const frames: any = [];
 
-
-    // Return a constant for each query.
-    const data = options.targets.map((target) => {
-      return new MutableDataFrame({
-        refId: target.refId,
-        fields: [
-          { name: 'Time', values: [from, to], type: FieldType.time },
-          { name: 'Value', values: [target.constant, target.constant], type: FieldType.number },
-        ],
-      });
+    const promises = options.targets.map((target) => {
+        const query = getTemplateSrv().replace(target.queryText, options.scopedVars);
+        return this.doRequest('/v2/export', {from, to, size: 1000, query }).then((response) => {
+        response.data?.lines?.forEach((line: any) => {
+          const frame = new MutableDataFrame({
+            refId: target.refId,
+            meta: {
+              preferredVisualisationType: 'logs',
+            },
+            fields: [
+              { name: 'time', type: FieldType.time },
+              { name: 'message', type: FieldType.string, labels: { app: line._app || '' } },
+              { name: 'level', type: FieldType.string },
+              { name: 'content', type: FieldType.string },
+              { name: 'app', type: FieldType.string }
+            ],
+          });
+          frame.add({time: line._ts, level: line.level, message: line.message || line.msg || line._line, app: line._app, content: line._line, labels: { app: line._app } })
+          frames.push(frame);
+        });
+      })
     });
-
-    return { data };
+    return Promise.all(promises).then(() => ({ data: frames }));
   }
 
   async testDatasource() {
     // Implement a health check for your data source.
-    const response = await this.doRequest('/v1/config/ingestion/status', {})
+    const response = await this.doRequest('/v1/config/ingestion/status')
 
     return {
       status: response.status,
