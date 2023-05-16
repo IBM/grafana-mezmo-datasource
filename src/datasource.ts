85dddd57e68a095ea5acf491d18304c7999c65c9
Sean Dague <sean.dague@ibm.com>
2023-05-16 15:06:15 -0400

    update logdna datasource with app tags

--- src/datasource.ts
+++ src/datasource.ts
@@ -27,9 +27,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
       url: this.url + "/example" + path,
       params
     }
-    console.log(req)
-    const result = await getBackendSrv().datasourceRequest(req)
-    console.log(result)
+    const result = await getBackendSrv().datasourceRequest(req);
     return result;
   }
 
@@ -37,30 +35,31 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     const { range } = options;
     const from = range!.from.valueOf();
     const to = range!.to.valueOf();
-    console.log("Targets", options.targets)
+    const frames: any = [];
 
     const promises = options.targets.map((target) => {
         const query = getTemplateSrv().replace(target.queryText, options.scopedVars);
         return this.doRequest('/v2/export', {from, to, size: 1000, query }).then((response) => {
-        const frame = new MutableDataFrame({
-          refId: target.refId,
-          fields: [
-            { name: 'time', type: FieldType.time },
-            { name: 'message', type: FieldType.string },
-            { name: 'level', type: FieldType.string },
-            { name: 'content', type: FieldType.string },
-            { name: 'app', type: FieldType.string}
-          ],
-        });
-        console.log("response");
-        console.log(response);
         response.data?.lines?.forEach((line: any) => {
-          frame.add({time: line._ts, level: line.level, message: line.message || line.msg || line._line, app: line._app, content: line._line })
+          const frame = new MutableDataFrame({
+            refId: target.refId,
+            meta: {
+              preferredVisualisationType: 'logs',
+            },
+            fields: [
+              { name: 'time', type: FieldType.time },
+              { name: 'message', type: FieldType.string, labels: { app: line._app || '' } },
+              { name: 'level', type: FieldType.string },
+              { name: 'content', type: FieldType.string },
+              { name: 'app', type: FieldType.string }
+            ],
+          });
+          frame.add({time: line._ts, level: line.level, message: line.message || line.msg || line._line, app: line._app, content: line._line, labels: { app: line._app } })
+          frames.push(frame);
         });
-        return frame;
       })
-  });
-    return Promise.all(promises).then((data) => ({ data }));
+    });
+    return Promise.all(promises).then(() => ({ data: frames }));
   }
 
   async testDatasource() {
